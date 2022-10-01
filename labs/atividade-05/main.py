2e6d267797cca340f4e88456be1206569c9309b4
Sergio Kenji Sawasaki Tanaka <serginho.tanaka@edu.unifil.br>
2022-09-30 23:26:26 -0300

    Atividade finalizada

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,31 +1,36 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    aux = 0
-    for i in range(len(figurinhas_da_maria)-1):
-        for j in range(len(figurinhas_do_joao)-1):
+    if len(figurinhas_do_joao) >= len(figurinhas_da_maria):
+        maior = figurinhas_do_joao
+        menor = figurinhas_da_maria
+    else:
+        menor = figurinhas_do_joao
+        maior = figurinhas_da_maria
 
-            if len(figurinhas_da_maria) >= len(figurinhas_do_joao):
-                if figurinhas_da_maria[i] == figurinhas_do_joao[j]:
-                    valor = figurinhas_da_maria[i]
-                    figurinhas_da_maria.remove(valor)
-                    print(valor)
-                    return len(figurinhas_da_maria)-1
-            if len(figurinhas_do_joao) > len(figurinhas_da_maria):
-                if figurinhas_do_joao[j] == figurinhas_da_maria[i]:
-                    valor = figurinhas_do_joao[j]
-                    figurinhas_do_joao[j].remove(valor)
+    maior_copy = maior.copy()
+    menor_copy = menor.copy()
+    for i in range(len(menor)):
+        for j in range(len(maior)):
 
-                if figurinhas_da_maria[i] != figurinhas_do_joao[j]:
-                    aux = aux + 1
+            if maior[j] == menor[i]:
+                for k in range(len(maior_copy)):
+
+                    if int(menor_copy[k]) == int(maior[j]):
+                        menor_copy.pop(k)
+                        if len(maior_copy) == 0:
+                            print(0)
+                        else:
+                            print(len(menor_copy))
+                        break
+            elif maior[j] is not menor[i]:
+                print(len(menor_copy))
+                break
 
 
 if __name__ == '__main__':
-    """""
+
     A, B = input().split(' ')
     figurinhas_da_maria = input().split(' ')
     figurinhas_do_joao = input().split(' ')
-    """
 
-    figurinhas_da_maria = [1, 3, 5, 7]
-    figurinhas_do_joao = [2, 3, 5]
 
     maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao)
